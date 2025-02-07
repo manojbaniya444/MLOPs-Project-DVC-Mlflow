24ae40ab8efe54a795b19a9ba214e1d79611e3c9
manojbaniya444 <86000102+manojbaniya444@users.noreply.github.com>
2025-02-07 13:13:30 +0545

    DVC: Model Building and Evvaluation

---
+++ src/model_evaluation.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+
+import pickle
+import json
+
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+test_data = pd.read_csv("./data/processed/test_processed.csv")
+
+X_test = test_data.iloc[:, 0:-1].values
+y_test = test_data.iloc[:, -1].values
+
+model = pickle.load(open("model.pkl", "rb"))
+
+y_pred = model.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+
+metrics_dict = {
+    "accuracy": accuracy,
+    "precision": precision,
+    "recall": recall,
+    "f1_score": f1
+}
+
+with open("metrics.json","w") as file:
+    json.dump(metrics_dict, file, indent=4)
